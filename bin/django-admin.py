765f8f2a85e43a42d66d9a1f122a4795cd42f088
yeashinr <y_rabbi@live.concordia.ca>
2019-01-23 12:57:55 -0500

    first commit

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/yeashin/Dev/Django_project/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ lib/python3.6/functools.py
@@ -0,0 +1,1 @@
+/Users/yeashin/anaconda/lib/python3.6/functools.py
