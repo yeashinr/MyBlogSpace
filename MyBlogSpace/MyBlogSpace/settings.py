af9b64e24f4709714285145813cff58b686a6aed
yeashinr <y_rabbi@live.concordia.ca>
2019-01-24 18:27:31 -0500

    create user registration form

--- MyBlogSpace/MyBlogSpace/settings.py
+++ MyBlogSpace/MyBlogSpace/settings.py
@@ -31,7 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
     'Blog.apps.BlogConfig',
+    'user.apps.UserConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
